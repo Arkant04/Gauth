213b77620a0b5a9d2d63437e260c5f0641a2ce87
ale <delacorte19@gmail.com>
2025-02-20 13:04:11 +0100

    #update google

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const port = 3000;
@@ -10,11 +11,23 @@ const port = 3000;
 
 const CLIENT_ID_GITHUB = process.env.CLIENT_ID_GITHUB;
 const CLIENT_SECRET_GITHUB = process.env.CLIENT_SECRET_GITHUB;
+const CLIENT_ID_GOOGLE = process.env.CLIENT_ID_GOOGLE;
+const CLIENT_SECRET_GOOGLE = process.env.CLIENT_SECRET_GOOGLE;
 
 passport.use(new GitHubStrategy({
   clientID: CLIENT_ID_GITHUB,
   clientSecret: CLIENT_SECRET_GITHUB,
   callbackURL: "http://localhost:3000/auth/github/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}
+));
+
+passport.use(new GoogleStrategy({
+  clientID: CLIENT_ID_GOOGLE,
+  clientSecret: CLIENT_SECRET_GOOGLE,
+  callbackURL: "http://localhost:3000/auth/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   return done(null, profile);
@@ -39,7 +52,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get("/", (req, res) => {
-  const html = "<a href='/auth/github'>Login con GitHub</a>";  
+  const html = "<a href='/auth/github'>Login con GitHub</a>"; 
   res.send(html);
 });
 
@@ -47,8 +60,19 @@ app.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ] })
 );
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: [ 'profile' ] })
+);
+
 app.get('/auth/github/callback',
     passport.authenticate('github', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/profile');
+    }
+);
+
+app.get('/auth/google/callback',  
+    passport.authenticate('google', { failureRedirect: '/' }),
     (req, res) => {
         res.redirect('/profile');
     }
